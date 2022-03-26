b3b9524080ddd1c1431b8b7651997c145e44a25c
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-03-26 18:45:13 +0600

    last-one

--- src/Component/ProductSummary/ProductSummary.js
+++ src/Component/ProductSummary/ProductSummary.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './ProductSummary.css'
 
 const ProductSummary = ({ cart }) => {
-    const { name, id } = cart;
+    const { name, id, img } = cart;
     return (
         <div className='cart'>
-            <h1>{name}</h1>
+            <p>{name}</p>
+            <img src={img} alt="" />
+
 
         </div>
 
--- src/Component/Writen/Writen.js
+++ src/Component/Writen/Writen.js
@@ -12,7 +12,12 @@ const Writen = () => {
                     Being a reusable component, it loads fast.External plugin can be used.Around 45% of worlds website use React major brands like Facebook,Instagram,Yahoo,NetFlix,Airbnb using React.</p>
             </div>
             <div className='useState'>
-                <h1>How to Work Usestate</h1>
+                <h1>How to Work UseState</h1>
+                <p>The state of your application is bound to change at some point. This could be the value of a variable, an object, or whatever type of data exists in your component.
+                    Hooks are a feature in React that allow you use state and other React features without writing classes.
+                    This website provides easy to understand code examples to help you learn how hooks work and inspire you to take advantage of them in your next project.
+                    Basically, what this hook does is that, it takes a parameter with value true or false and toggles that value to opposite.
+                    It's useful when we want to take some action into it's opposite action, for example: show and hide modal, show more/show less text, open/close side menu.</p>
             </div>
 
         </div>
